3014530cc4b16f5fde6d1a57293019d6e53b88d4
breakok <95564840+breakok@users.noreply.github.com>
2022-02-18 17:05:45 +0530

    Create anj.sh

---
+++ anj.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DNeSzoG5s8czJdjMi2DziHTBACZuPeq9G2.teamajag#svo1-y3gf --ethstratum ETHPROXY
